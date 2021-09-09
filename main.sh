a96415f70fe23cf81db231992dd2c5f4d2ede15b
traaitt | TRRXITTE inc <traaitt@icloud.com>
2021-09-09 06:00:52 +0200

    Initial commit

---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+# static explorer page generator
+# (c)) TRRXITTE Int., incorporated, TRTL Team | 2020 - TODAY
+
+# see LICENSE for details
+
+HEIGHT=$(curl -s http://localhost:24496/getheight | jq '.height')
+let "HEIGHT--"
+
+cat ascii.txt
+
+echo -e "\n### CURRENT BLOCK ###"
+curl -s http://localhost:24496/getheight | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]'
+
+echo -e "\n### LAST BLOCK HEADER ###"
+curl -sd '{"jsonrpc":"2.0","method":"getlastblockheader","params":{}}' http://localhost:24496/json_rpc | jq -r .result.block_header | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]'
+
+echo -e "\n### NETWORK INFO ###"
+curl -s http://localhost:24496/getinfo | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]' 
+
+echo -e "\n### PENDING TRANSACTIONS ###"
+curl -sd '{"jsonrpc":"2.0","method":"f_on_transactions_pool_json","params":{}}' http://localhost:24496/json_rpc | jq '.result.transactions[]' | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]' 
+
+echo -e "\n### LAST 10 BLOCKS ###"
+curl -sd '{"jsonrpc":"2.0","method":"f_blocks_list_json","params":{"height":'"$HEIGHT"'}}' http://localhost:24496/json_rpc | jq '.result.blocks' | jq '.[0:10]'
+
+cat footer.txt
